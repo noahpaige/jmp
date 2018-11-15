d2dd951b8e9deeb34853c962f4969bf0679b902b
Noah Paige <noahpaige@Noahs-MacBook-Air.local>
2018-11-15 10:52:59 -0800

    Floor and box spawning
    Added a static, collide-able floor.
    Added dynamicBody box spawning at location touched.

--- core/src/com/mygdx/game/Constants.kt
+++ core/src/com/mygdx/game/Constants.kt
@@ -1,7 +1,10 @@
 package com.mygdx.game
+import com.badlogic.gdx.math.Vector2
+
 
 class Constants {
     companion object {
         const val PIXELS_PER_METER = 32F
+        const val SCREEN_MIDDLE_X = 12.5F
     }
 }
--- core/src/com/mygdx/game/JmpGame.kt
+++ core/src/com/mygdx/game/JmpGame.kt
@@ -3,10 +3,13 @@ package com.mygdx.game
 import com.badlogic.ashley.core.*
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.physics.box2d.*
 import com.google.inject.*
 
@@ -23,14 +26,18 @@ class JmpGame : ApplicationAdapter() {
         injector = Guice.createInjector(GameModule(myGdxGame = this))
         injector.getInstance(Systems::class.java).list.map {
             injector.getInstance(it)}.forEach{ system -> engine.addSystem(system)}
+
         createEntities()
+        //use inpult multiplexor to manage inputs from UI vs game
+        Gdx.input.inputProcessor = injector.getInstance(MyInputAdapter::class.java)
     }
 
     private fun createEntities(){
         val world = injector.getInstance(World::class.java)
+        //player entity
         engine.addEntity(Entity().apply{
             add(TextureComponent(img))
-            add(TransformComponent(Vector2(12.5F,20F)))
+            add(TransformComponent(Vector2(Constants.SCREEN_MIDDLE_X,20F)))
 
             val body = world.createBody(BodyDef().apply {
                 type = BodyDef.BodyType.DynamicBody
@@ -41,6 +48,18 @@ class JmpGame : ApplicationAdapter() {
             body.setTransform(transform.position, 0F)
             add(PhysicsComponent(body))
 
+        })
+        //floor entity
+        engine.addEntity(Entity().apply {
+            add(TransformComponent(Vector2(Constants.SCREEN_MIDDLE_X,0f)))
+            val body = world.createBody(BodyDef().apply {
+                type = BodyDef.BodyType.StaticBody
+            })
+            body.createFixture(PolygonShape().apply {
+                setAsBox(20F, 1F)
+            }, 1.0F)
+            body.setTransform(transform.position, 0F)
+            add(PhysicsComponent(body))
         })
     }
 
@@ -54,6 +73,37 @@ class JmpGame : ApplicationAdapter() {
     override fun dispose() {
         batch.dispose()
         img.dispose()
+    }
+}
+
+
+class MyInputAdapter @Inject constructor(private val camera: OrthographicCamera,
+                                         private val engine: Engine,
+                                         private val world: World) : InputAdapter() {
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        println("ScreenX: $screenX        ScreenY: $screenY")
+        val worldPosition = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(),0f))
+        println("WorldX: ${worldPosition.x}        WorldY: ${worldPosition.y}")
+
+
+        //
+        engine.addEntity(Entity().apply{
+            add(TransformComponent(Vector2(worldPosition.x, worldPosition.y)))
+
+            val body = world.createBody(BodyDef().apply {
+                type = BodyDef.BodyType.DynamicBody
+            })
+            body.createFixture(PolygonShape().apply {
+                setAsBox(1F, 1F)
+            }, 1.0F)
+            body.setTransform(transform.position, 0F)
+            add(PhysicsComponent(body))
+
+        })
+        //
+
+        return true
+
     }
 }
 
