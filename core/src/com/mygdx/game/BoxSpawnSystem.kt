10641d9440507565c73493cec4e368500392693c
Noah Paige <noahlandonpaige@gmail.com>
2018-12-04 17:31:53 -0800

    forgot to add this

--- core/src/com/mygdx/game/BoxSpawnSystem.kt
+++ core/src/com/mygdx/game/BoxSpawnSystem.kt
@@ -4,8 +4,10 @@ import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
 import com.badlogic.gdx.physics.box2d.BodyDef
 import com.badlogic.gdx.physics.box2d.PolygonShape
 import com.badlogic.gdx.physics.box2d.World
@@ -22,9 +24,10 @@ class BoxSpawnSystem @Inject constructor(private val camera: OrthographicCamera,
     override fun update(deltaTime: Float) {
         if(counter > 2) {
             counter = 0f
-            val pos = Vector2(Random().nextFloat() * (maxX - minX) + minX, 50f)
+            val pos = Vector2(Random().nextFloat() * (maxX - minX) + minX, camera.position.y + 30f)
             engine.addEntity(Entity().apply{
                 add(TextureRegionComponent(TextureRegion(JmpGame.img)))
+                //add(TextureComponent(JmpGame.img))
                 add(TransformComponent(Vector2(pos.x, pos.y), 0F, 0.25F))
 
                 val body = world.createBody(BodyDef().apply {
@@ -36,7 +39,7 @@ class BoxSpawnSystem @Inject constructor(private val camera: OrthographicCamera,
                 body.setTransform(transform.position, 0F)
                 body.isFixedRotation = true
                 add(PhysicsComponent(body))
-                body.userData = EntityData("block", false)
+                body.userData = EntityData("block", mutableListOf<Body>(), false)
                 body.gravityScale = 0.2f
 
                 //body.setLinearVelocity(0f, -10f)
